f95cfbd5b876adc5ea1f4dfe78c8c4a3054c5253
Michael Cruz <michael.cruz@uwalumni.com>
2017-08-07 18:08:27 -0700

    initial commit

---
+++ spec/plug-sqlog-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PlugSqlogView from '../lib/plug-sqlog-view';
+
+describe('PlugSqlogView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
